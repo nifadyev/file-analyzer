9f43cddd685b0b745f742337de2a6ef1df70b72a
Vadim Nifadyev <vnifadyev@gmail.com>
2019-05-01 15:24:48 +0300

    Add tags number characteristic, fix wrong counting
    - Count only opening and closing tags without attributes
    - Fix wrong number of words and number of chars in useful information

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ file_analyzer = FileAnalyzer(sys.argv[1:])
 
 # HTML file features (or characteristics)
 print('Number of chars (markup information):', file_analyzer.markup_information_symbols)
-# print('Number of tags (markup information):', )
+print('Number of tags (markup information):', file_analyzer.markup_information_words)
 print('Number of chars (displayed information):', file_analyzer.useful_information_symbols)
 print('Number of words (displayed information):', file_analyzer.useful_information_words)
 print('Displayed information to markup information ratio:',
--- src/file_analyzer.py
+++ src/file_analyzer.py
@@ -8,9 +8,12 @@ import argparse
 # TODO: count css
 # TODO: выполнить выборку из например 1000 сайтов и файлов, сравнить, мб построить графики
 # Привести общую инфу по форматам и сравнить их харки
+
+
 class FileAnalyzer:
     """Analyze markup files and show their various characteristics."""
 
+    # This fields are the same to class
     _response_body = None
     _markup_information = None
     _useful_information = None
@@ -18,6 +21,7 @@ class FileAnalyzer:
     _soup = None
 
     def __init__(self, args):
+        # This one are unique to each class entity
         self.arguments = self._parse_arguments(args)
         self.uri = self.arguments.file_path
         self._scrape()
@@ -28,12 +32,20 @@ class FileAnalyzer:
 
     def _scrape(self):
         # TODO: decode response (get rid of such shit - &#1055;)
-        self._response_body = requests.get(self.uri, headers={'Content-Type': 'text/html;charset=UTF-8'}).text
+        # self._response_body = requests.get(
+            # self.uri, headers={'Content-Type': 'text/html;charset=UTF-8'}).text
+
+        with open(self.uri, 'r') as html:
+            self._response_body = html.read()
+
+        # print(self._response_body)
 
         # TODO: add meaning of regular expression
         self._markup_information = re.findall(
             r'<+/?.*?>+', self._response_body
         )
+
+        # print(self._markup_information)
 
         #  Write only useful text to file
         soup = BeautifulSoup(self._response_body, "html.parser")
@@ -66,6 +78,7 @@ class FileAnalyzer:
         # drop blank lines
         self._useful_information = '\n'.join(
             chunk for chunk in chunks if chunk)
+        # print(self._useful_information)
 
     def validate_file_path(self, file_path):
         """ """
@@ -90,7 +103,8 @@ class FileAnalyzer:
             argparse.Namespace -- parsed arguments of valid type.
         """
 
-        argument_parser = argparse.ArgumentParser(description="Markup file analyzer")
+        argument_parser = argparse.ArgumentParser(
+            description="Markup file analyzer")
 
         argument_parser.add_argument(
             "file_path", help="path to local file or URL", type=self.validate_file_path
@@ -116,16 +130,38 @@ class FileAnalyzer:
     def markup_information_symbols(self):
         return sum((len(line) for line in self._markup_information))
 
-    # @property
-    # def markup_information_words(self):
+    @property
+    def markup_information_words(self):
+        # ? How to count attr values if they are grouped using comma
+        # ? Also how to get rid of '=' cause its not wrapped with spaces
+
+        # length = 0
+        # for tag in self._markup_information:
+        #     print(tag.split(), len(tag.split()))
+        #     tags_and_attrs = tag.split()
+        #     if len(tags_and_attrs) == 1:
+        #         length += len(tags_and_attrs)
+        #     else:
+        #         length += sum(sum(len(w) for w in word.split('=')) for word in tags_and_attrs)
+            # length += len(tag.split().split('='))
+
+        # Count both opening and closing tags WITHOUT attributes
+        return len(self._markup_information)
+        # return length
 
     @property
     def useful_information_symbols(self):
-        return sum((len(line) for line in self._useful_information))
+        # Remove '\n' from total number of markup symbols
+        # Last string doesn't have trailing '\n'
+        return len(self._useful_information) - len(self._useful_information.splitlines()) + 1
 
     @property
     def useful_information_words(self):
-        return len(self._useful_information.split(' '))
+        # FIXME: cannot count words if there are more than 1 space
+        # FIXME: dont count words correctly because last word in Title wont count
+        # ! Thats why use hardcoded +1 length
+
+        return len(self._useful_information.split(' ')) + 1
 
     @property
     def useful_info_to_markup_info_ratio(self):
