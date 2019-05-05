5e4d7ff8ee016e90f3792cd36c596fb6061f6904
Vadim Nifadyev <vnifadyev@gmail.com>
2019-05-05 19:22:56 +0300

    Add simple xml parser

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ file_analyzer = FileAnalyzer(sys.argv[1:])
 
 
 # HTML file features (or characteristics)
+print('Total number of chars: ', file_analyzer.total_symbols_number)
 print('Number of chars (markup information):', file_analyzer.markup_information_symbols)
 print('Number of tags (markup information):', file_analyzer.markup_information_words)
-print('Number of chars in script tags (markup information):', file_analyzer.markup_information_script)
-print('Number of chars in style tags (markup information):', file_analyzer.markup_information_style)
+# print('Number of chars in script tags (markup information):', file_analyzer.markup_information_script)
+# print('Number of chars in style tags (markup information):', file_analyzer.markup_information_style)
 print('Number of chars (displayed information):', file_analyzer.useful_information_symbols)
 print('Number of words (displayed information):', file_analyzer.useful_information_words)
 print('Displayed information to markup information ratio:',
--- src/file_analyzer.py
+++ src/file_analyzer.py
@@ -1,13 +1,13 @@
 #  -*- coding: utf-8 -*-
-import requests
 import re
-from bs4 import BeautifulSoup
 import argparse
+import requests
+from bs4 import BeautifulSoup
 
-# TODO: count script
-# TODO: count css
 # TODO: выполнить выборку из например 1000 сайтов и файлов, сравнить, мб построить графики
 # Привести общую инфу по форматам и сравнить их харки
+# !!! TODO: WRITE TESTS USING PYTEST
+# TODO: get rid of too many if else
 
 
 class FileAnalyzer:
@@ -19,35 +19,92 @@ class FileAnalyzer:
     _useful_information = None
     _script = None
     _style = None
-    _url = False
+    # _url = False
     _soup = None
 
     def __init__(self, args):
         # This one are unique to each class entity
         self.arguments = self._parse_arguments(args)
         self.uri = self.arguments.file_path
-        self._scrape()
+        self.is_html = False
+        # ? Maybe do this that way
+        # self.response = None
+
+        # TODO: move this if else to separate function
+        # Very simple check for whether uri is url or path to file
+        # ! Will fail if file is in '.htm' format
+        if self.uri.endswith('.html')\
+                and not self.uri.startswith('http')\
+                and not self.uri.startswith('www'):
+            self.is_html = True
+            with open(self.uri, 'r') as html:
+                self._response_body = html.read()
+        # ! Fill fail if filename starts with these prefixes
+        elif self.uri.startswith('http') or self.uri.startswith('www'):
+            # TODO: decode response (get rid of such shit - &#1055;)
+            self.is_html = True
+            self._response_body = requests.get(
+                self.uri,
+                headers={'Content-Type': 'text/html;charset=UTF-8'}
+            ).text
+
+        if self.uri.endswith('.xml')\
+                and not self.uri.startswith('http')\
+                and not self.uri.startswith('www'):
+            with open(self.uri) as xml:
+                self._response_body = xml.read()
+        elif self.uri.startswith('http') or self.uri.startswith('www'):
+            self._response_body = requests.get(
+                self.uri,
+                headers={'Content-Type': 'text/xml;charset=UTF-8'}
+            ).text
+
+        if self.is_html:
+            self._html_scrape()
+            self.is_html = False
+        else:
+            self._xml_scrape()
+
         self.write_response('results/response.html')
         self.write_markup_information('results/tags.txt')
         self.write_useful_information('results/useful_info.txt')
         # self.request = requests.get(uri)
 
-    def _scrape(self):
-        # Very simple check for whether uri is url or path to file
-        if self.uri.endswith('.html') and not self.uri.startswith('http')\
-                and not self.uri.startswith('www'):
 
-            with open(self.uri, 'r') as html:
-                self._response_body = html.read()
-
-        elif self.uri.startswith('http') and self.uri.startswith('www'):
-            # TODO: decode response (get rid of such shit - &#1055;)
-            self._response_body = requests.get(
-                self.uri, headers={'Content-Type': 'text/html;charset=UTF-8'}).text
-
-        # TODO: add meaning of regular expression
+    def _xml_scrape(self):
+        print(self._response_body)
         self._markup_information = re.findall(
-            r'<+/?.*?>+', self._response_body
+            r'<+/?.*?>+',  # TODO: add meaning of regular expression
+            self._response_body
+        )
+
+        #  Write only useful text to file
+        # ! Pronably invalid. Check how to extract info from xml
+        # soup = BeautifulSoup(self._response_body, "xml.parser")
+        soup = BeautifulSoup(self._response_body, "html.parser")
+        self._soup = soup
+
+        # get text
+        # text = soup.get_text()
+        # ? Why separator is being used
+        self._useful_information = soup.get_text(separator=' ')
+
+        # break into lines and remove leading and trailing space on each
+        lines = (line.strip()
+                for line in self._useful_information.splitlines())
+        # break multi-headlines into a line each
+        # ? For what
+        chunks = (phrase.strip()
+                for line in lines for phrase in line.split("  "))
+        # drop blank lines
+        self._useful_information = '\n'.join(
+            chunk for chunk in chunks if chunk)
+
+    def _html_scrape(self):
+        # ! Example of scraping xml tags is presented in regex cheatsheet
+        self._markup_information = re.findall(
+            r'<+/?.*?>+',  # TODO: add meaning of regular expression
+            self._response_body
         )
 
         # print(self._markup_information)
@@ -59,12 +116,15 @@ class FileAnalyzer:
         # print([tag.name for tag in soup.find_all()])
 
         # quiet straight forward solution, change in future
-        script_with_empty_strings = [line.strip() for line in soup.find('script').text.splitlines() if line]
+        script_with_empty_strings = [
+            line.strip() for line in soup.find('script').text.splitlines() if line
+        ]
         self._script = [line for line in script_with_empty_strings if line]
         # print(self._script)
 
-        # self._style = soup.find('style').text
-        style_with_empty_strings = [line.strip() for line in soup.find('style').text.splitlines() if line]
+        style_with_empty_strings = [
+            line.strip() for line in soup.find('style').text.splitlines() if line
+        ]
         self._style = [line for line in style_with_empty_strings if line]
         print(self._style)
         # Remove script and style elements
@@ -73,22 +133,22 @@ class FileAnalyzer:
 
         # get text
         # text = soup.get_text()
+        # ? Why separator is being used
         self._useful_information = soup.get_text(separator=' ')
-        # text = soup.body.get_text(separator= ' ')
 
         # break into lines and remove leading and trailing space on each
         lines = (line.strip()
-                 for line in self._useful_information.splitlines())
+                for line in self._useful_information.splitlines())
         # break multi-headlines into a line each
+        # ? For what
         chunks = (phrase.strip()
-                  for line in lines for phrase in line.split("  "))
+                for line in lines for phrase in line.split("  "))
         # drop blank lines
         self._useful_information = '\n'.join(
             chunk for chunk in chunks if chunk)
-        # print(self._useful_information)
 
     def validate_file_path(self, file_path):
-        """ """
+        """Check file path for validity."""
 
         # TODO: regex for check if file_path is valid url
         # if url:
@@ -121,16 +181,6 @@ class FileAnalyzer:
         #     action='store_true'
         # )
 
-        # def raise_value_error(err_msg):
-        #     raise argparse.ArgumentTypeError(err_msg)
-
-        # argument_parser.error = raise_value_error
-
-        # try:
-        #     return argument_parser.parse_args(args)
-        # except BaseException:
-        #     print(sys.exc_info()[1])
-        #     sys.exit()
         return argument_parser.parse_args(args)
 
     @property
@@ -158,34 +208,49 @@ class FileAnalyzer:
 
     @property
     def markup_information_script(self):
-        # Count chars of JS code without tabs
+        """Count amount of JS code in chars (without tabs)."""
+
         return sum(len(line) for line in self._script)
 
     @property
     def markup_information_style(self):
-        # Count chars of CSS styles without tabs
+        """Count amount of CSS styles in chars."""
+
         return sum(len(line) for line in self._style)
 
     @property
     def useful_information_symbols(self):
+        """Count amount of useful (displayed to user) information in chars."""
+
         # Remove '\n' from total number of markup symbols
         # Last string doesn't have trailing '\n'
-        return len(self._useful_information) - len(self._useful_information.splitlines()) + 1
+        return len(self._useful_information)\
+            - len(self._useful_information.splitlines())\
+            + 1
 
     @property
     def useful_information_words(self):
+        """Count amount of useful (displayed to user) information in words."""
         # FIXME: cannot count words if there are more than 1 space
+        # TODO: remove this wrapping spaces while scraping
+
         # FIXME: dont count words correctly because last word in Title wont count
         # ! Thats why use hardcoded +1 length
-
-        return len(self._useful_information.split(' ')) + 1
+        return len(self._useful_information.split(' '))\
+            + 1
 
     @property
     def useful_info_to_markup_info_ratio(self):
+        """Count ratio of displayed information to markup information."""
+
         return self.useful_information_symbols / self.markup_information_symbols
 
     def write_response(self, output_file):
-        """ """
+        """Write response to file.
+
+        Arguments:
+            output_file {str} -- path to output file.
+        """
 
         with open(output_file, 'w+', encoding="utf-8") as output:
             # str_list = list(filter(None, request.text))
@@ -198,18 +263,27 @@ class FileAnalyzer:
             output.write(self._soup.prettify().replace('&amp;', '&'))
 
     def write_useful_information(self, output_file_path):
+        """Write us useful information to file.
+
+        Arguments:
+            output_file {str} -- path to output file.
+        """
         with open(output_file_path, 'w+', encoding='utf-8') as output:
             output.write(self._useful_information)
 
     def write_markup_information(self, output_file_path):
+        """Write markup information to file.
+
+        Arguments:
+            output_file {str} -- path to output file.
+        """
         with open(output_file_path, 'w+') as output:
             for string in self._markup_information:
                 output.write(string+'\n')
 
-    # TODO: read about properties
-    # @property
-    def get_total_symbols_amount(self):
-        return sum((len(line) for line in self.request.text))
+    @property
+    def total_symbols_number(self):
+        return len(self._response_body)
 
     # @property
     # def get_uri(self):
