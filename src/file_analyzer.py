6a837dd7c6eaf058e9d6071b15af1f0827bd8890
Vadim Nifadyev <vnifadyev@gmail.com>
2020-09-15 19:39:30 +0300

    Remove TODOs and FIXs

--- src/file_analyzer.py
+++ src/file_analyzer.py
@@ -40,7 +40,6 @@ class FileAnalyzer:
     def define_file_format(file_path):
         """Return file format and flag if file is local."""
         # Very simple check for whether uri is url or path to file
-        # ! Will fail if file is in '.htm' format
         if (
                 file_path.endswith('.html')
                 and not file_path.startswith('http')
@@ -49,9 +48,7 @@ class FileAnalyzer:
             file_format = 'html'
             is_local = True
 
-        # ! Fill fail if filename starts with these prefixes
         elif file_path.startswith('http') or file_path.startswith('www'):
-            # TODO: decode response (get rid of such shit - &#1055;)
             file_format = 'html'
             is_local = False
         elif (
@@ -92,37 +89,22 @@ class FileAnalyzer:
 
     def _xml_scrape(self):
         self._markup_information = re.findall(r'<+/?.*?>+', self._response_body)
-
-        #  Write only useful text to file
-        # ! Pronably invalid. Check how to extract info from xml
-        # soup = BeautifulSoup(self._response_body, "xml.parser")
-        # ! Need lxml to parse XML
         soup = BeautifulSoup(self._response_body, 'html.parser')
         self._soup = soup
 
         tags = Counter((tag.name for tag in soup.find_all()))
-        # Get first tuple from most common tags list
-        # And tag name from tuple
+        # Get first tuple from most common tags list and tag name from tuple
         self._most_common_tag = tags.most_common(1)[0][0]
-
         self._useful_information = soup.get_text(separator=' ')
 
-        # break into lines and remove leading and trailing space on each
-        lines = (
-            line.strip() for line in self._useful_information.splitlines()
-        )
-        # break multi-headlines into a line each
-        # ? For what
-        chunks = (
-            phrase.strip() for line in lines for phrase in line.split("  ")
-        )
+        lines = (line.strip() for line in self._useful_information.splitlines())
+
+        chunks = (phrase.strip() for line in lines for phrase in line.split('  '))
         # drop blank lines
-        self._useful_information = '\n'.join(
-            chunk for chunk in chunks if chunk)
+        self._useful_information = '\n'.join(chunk for chunk in chunks if chunk)
 
     def _get_keys_and_values_json(self, input_json_file, destination):
         if isinstance(input_json_file, list):
-            # ! HARDCODE, fix it
             if input_json_file:
                 if isinstance(input_json_file[0], dict):
                     for value in input_json_file:
@@ -140,13 +122,8 @@ class FileAnalyzer:
                     destination.append(str(value))
 
     def _json_scrape(self):
-        # ! Doesnt handle such values^ {ip}
-        # ! Doesn't count /t and spaces
         self._markup_information = re.findall(
-            # TODO: add meaning of regular expression
-            r'(\t{1,}|^\s{4,}|"|:|{|}|\[|\]|\,)',
-            self._response_body
-        )
+            r'(\t{1,}|^\s{4,}|"|:|{|}|\[|\]|\,)', self._response_body)
 
         spaces = 0
         for char in self._response_body:
@@ -160,20 +137,17 @@ class FileAnalyzer:
         self._get_keys_and_values_json(parsed_json, self._useful_information)
 
     def _html_scrape(self):
-        #  Write only useful text to file
-        soup = BeautifulSoup(self._response_body, "html.parser")
+        soup = BeautifulSoup(self._response_body, 'html.parser')
         self._soup = soup
 
         tags = Counter((tag.name for tag in soup.find_all()))
-        # Get first tuple from most common tags list
-        # And tag name from tuple
+        # Get first tuple from most common tags list and tag name from tuple
         self._most_common_tag = tags.most_common(1)[0][0]
-        # quiet straight forward solution, change in future
+
         scripts = soup.find_all('script')
         if scripts:
             self._script = []
             for script in scripts:
-                # ! str(line).strip() need to remove empty strings
                 self._script.extend(
                     line + '\n' for line in script.text.splitlines() if str(line).strip())
                 if self._script:
@@ -183,13 +157,10 @@ class FileAnalyzer:
         if styles:
             self._style = []
             for style in styles:
-                # ! str(line).strip() need to remove empty strings
                 self._style.extend(
                     line + '\n' for line in style.text.splitlines() if str(line).strip())
-                # self._style[-1] = re.sub('\n', '', self._style[-1])
 
-        # Remove script and style elements
-        for extra in soup(["script", "style"]):
+        for extra in soup(['script', 'style']):
             extra.extract()
 
         # Break into lines, remove leading and trailing space on each
@@ -203,7 +174,8 @@ class FileAnalyzer:
             response_body_copy = re.sub(line, '\1', response_body_copy)
         self._markup_information = response_body_copy
 
-    def _parse_arguments(self, args):
+    @staticmethod
+    def _parse_arguments(args):
         """Handle command line arguments using argparse.
 
         Arguments:
@@ -215,8 +187,7 @@ class FileAnalyzer:
         Returns:
             argparse.Namespace -- parsed arguments of valid type.
         """
-        argument_parser = argparse.ArgumentParser(
-            description="Markup file analyzer")
+        argument_parser = argparse.ArgumentParser(description='Markup file analyzer')
 
         argument_parser.add_argument('file_path', help='path to local file or URL')
 
@@ -228,25 +199,16 @@ class FileAnalyzer:
 
     @property
     def markup_information_words(self):
-        # ! Propably wont find inline tags
-        return len(re.findall(
-                # ! (?#comment) in regex
-                # Opening tags with leading tabs or spaces
-                r'<[^/?][^>]+>',
-                self._response_body
-            )
-        )
+        return len(re.findall(r'<[^/?][^>]+>', self._response_body))
 
     @property
     def markup_information_script(self):
         """Count amount of JS code in chars."""
-        # Also count '\n' symbols
         return sum(len(line) for line in self._script)
 
     @property
     def markup_information_style(self):
         """Count amount of CSS styles in chars."""
-        # Also count '\n' symbols
         return sum(len(line) for line in self._style) + len(self._style) - 1
 
     @property
@@ -257,22 +219,18 @@ class FileAnalyzer:
     @property
     def useful_information_words(self):
         """Count amount of useful (displayed to user) information in words."""
-        # FIXME: cannot count words if there are more than 1 space
-        # TODO: remove this wrapping spaces while scraping
-
-        # FIXME: dont count words correctly because last word in Title wont count
-        # Split each line into separate words
-        # ! Check for empty linesREGEX
         words = tuple(
-            phrase.strip() for line in self._useful_information for phrase in line.split()
-        )
+            phrase.strip() for line in self._useful_information for phrase in line.split())
 
         return len(words)
 
     @property
     def useful_info_to_markup_info_ratio(self):
         """Count ratio of displayed information to markup information."""
-        return self.useful_information_symbols / (self.total_symbols_number - self.useful_information_symbols)
+        return (
+            self.useful_information_symbols
+            / (self.total_symbols_number - self.useful_information_symbols)
+        )
 
     @property
     def most_common_tag(self):
